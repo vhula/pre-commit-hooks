2a7af7804c9ad2269b68f66fc868b3772f5b2da5
Vadym Hula <vadym.hula@gmail.com>
2023-06-17 15:13:22 +0300

    Add installation messages

--- install-pre-commit-hook.sh
+++ install-pre-commit-hook.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Installing gitleaks pre-commit hook..."
+
 git config --local --bool --add gitleaks.enabled true
 
 curl --silent https://raw.githubusercontent.com/vhula/pre-commit-hooks/main/gitleaks.sh > .git/hooks/pre-commit
 
 chmod a+x .git/hooks/pre-commit
 
+echo "Gitleaks pre-commit hook has been installed and enabled."
